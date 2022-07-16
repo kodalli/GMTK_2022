74efd6285de04b43d859ad0234735cdb1ce83a55
Surya Tej Kodali <surya.kodali@gmail.com>
2022-07-16 15:51:27 -0400

    dialogue system code and wall tiles

--- Assets/Scripts/Card/CardRenderer.cs
+++ Assets/Scripts/Card/CardRenderer.cs
@@ -1,11 +1,20 @@
 ﻿using System;
+using TMPro;
 using UnityEngine;
 
 namespace Card {
     public class CardRenderer : MonoBehaviour {
+
+        public GameObject title;
+        public GameObject description;
+        
         // Put the sprite for the card on the ui after rolling and place its props in text on it
-        public void render(Card card) {
-            throw new NotImplementedException();
+        public void Render(Card card) {
+            var titleText = title.GetComponent<TextMeshPro>();
+            titleText.SetText(card.statusName);
+            
+            var descriptionText = title.GetComponent<TextMeshPro>();
+            var details = card.effectDescription + "\n" + card.effectStrength + "\n" + card.appliedTo;
         }
     }
 }
