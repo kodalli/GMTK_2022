d461e2ff2c9427e3b06b50d8fc2f4afb68fa0980
suraj kopparam <surajmsd1@gmail.com>
2022-07-16 23:39:41 -0400

    working 3.1

--- Assets/HomingToPlayer.cs
+++ Assets/HomingToPlayer.cs
@@ -8,10 +8,12 @@ public class HomingToPlayer : MonoBehaviour
 
     [SerializeField]
     private float speed = 6f;
+    private bool updateOn = true;
     void Start()
     {
         player = GameObject.FindGameObjectWithTag("Player");
         Invoke("toPlayer", 3f);
+        StartCoroutine(updateOff());
     }
 
     private void toPlayer()
@@ -19,5 +21,19 @@ public class HomingToPlayer : MonoBehaviour
         Vector2 face =(player.transform.position - transform.position).normalized;
         this.GetComponent<Rigidbody2D>().AddForce(face * speed, ForceMode2D.Impulse);
         Destroy(this, 3f);
+    }
+    private void Update()
+    {
+        if (updateOn == true)
+        {
+            Vector2 face = (player.transform.position - transform.position).normalized;
+
+        }
+        
+    }
+    IEnumerator updateOff()
+    {
+        yield return new WaitForSeconds(5.0f);
+        updateOn = false;
     }
 }
--- Assets/Scripts/BulletScripts/CircleMoveTogether.cs
+++ Assets/Scripts/BulletScripts/CircleMoveTogether.cs
@@ -12,19 +12,17 @@ public class CircleMoveTogether : MonoBehaviour
     [SerializeField]    
     private float projectileSpeed = 4f;
 
+    private GameObject player;
     void Start()
     {
+        player = GameObject.FindGameObjectWithTag("Player");
         InvokeRepeating("TriangleSummon",0f,1f);
     }
     private void TriangleSummon()
     {
+
         GameObject bullet = Instantiate(TrianglesPrefab, transform);
       //  bullet.transform.position 
         bullet.GetComponent<Rigidbody2D>().AddForce(TriangleMoveDir * projectileSpeed, ForceMode2D.Impulse);
-        
-
-        Destroy(bullet, 4f);
-
-        
     }
 }
