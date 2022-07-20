e06bdf6e6df63f4facfa6be6b50942ea883fbc53
Surya Tej Kodali <surya.kodali@gmail.com>
2022-07-19 22:56:25 -0400

    moved status effects to separate file

---
+++ Assets/Scripts/Card/ApplyStatusEffects.cs
@@ -0,0 +1,33 @@
+﻿using System.Linq;
+
+namespace Card {
+    
+    public class ApplyStatusEffects {
+        public void ApplyPlayer(ref int damageBoost, ref int fireRate, ref int durability) {
+            var effects = GameManager.Instance.activeStatuses;
+            foreach (var card in effects.Where(card => card.appliedTo.Equals("Timmy"))) {
+                switch (card.statusName) {
+                    case "Heavy Fire":
+                        damageBoost += card.effectStrength;
+                        break;
+                    case "Quickshot":
+                        fireRate += card.effectStrength;
+                        break;
+                    case "Hardened":
+                        durability += card.effectStrength;
+                        break;
+                    case "Brittle":
+                        durability += -card.effectStrength;
+                        break;
+                    case "Jammed":
+                        fireRate += -card.effectStrength;
+                        break;
+                    case "Weak":
+                        damageBoost += -card.effectStrength;
+                        break;
+                }
+            }
+        }
+ 
+    }
+}
